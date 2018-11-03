25cd1f3e3e87474fc13cfeb18f776796d39edafa
Zylox <zch5of6@hotmail.com>
2018-11-03 12:37:33 -0500

    vgo sucks for windows with vscode right now

--- go/cmd/dispatch/main.go
+++ go/cmd/dispatch/main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	// "github.com/aws/aws-sdk-go/aws"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
 	// "github.com/nlopes/slack"
 )
 
+func GetSecret(sess *session.Session, secretID string) *string {
+	sm := secretsmanager.New(sess)
+	input := &secretsmanager.GetSecretValueInput{SecretId: aws.String(secretID)}
+	// output, err := sm.GetSecret
+	secretsmanager.GetSecretValue(input)
+}
+
+func HandleRequest(ctx context.Context, name interface{}) (string, error) {
+	return fmt.Sprintf("Hello %s!", ""), nil
+}
+
 func main() {
-	fmt.Printf("ok")
+	// slackOauth :=
+
+	lambda.Start(HandleRequest)
 }
